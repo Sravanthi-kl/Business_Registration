7c4fc66afce44ed6c34fad83bce2d850e891d3b4
Sravanthi <sravanthi.kl@strata.co.in>
2015-06-19 14:31:42 +0530

    Dropdown showing blank value before selection for City and Branch type

--- app/models/jb_branch.rb
+++ app/models/jb_branch.rb
@@ -1,7 +1,7 @@
 class JbBranch < ActiveRecord::Base
   attr_accessible :branchaddress, :branchname, :branchtype, :city, :contactnumbers, :maplink, :shortname, :workinghours
 
-   validates :branchaddress,:branchname,:contactnumbers,:workinghours,:maplink,:presence => true
+   validates :branchaddress,:branchname,:contactnumbers,:branchtype,:city,:workinghours,:maplink,:presence => true
    validates :branchaddress,:branchname,:shortname, :length => { :maximum =>10 }
    validates :branchname, :uniqueness => true
    validates :contactnumbers, format: { with: /\d{3}-\d{3}-\d{4}/, message: "Valid Format :xxx-xxx-xxx" }
